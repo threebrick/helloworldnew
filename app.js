44dca30dd9dde2dd2be1f65726f52c8a2c54495a
threebrick <darnell@threebrick.com>
2016-09-10 00:17:29 -0400

    Updated app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ server.listen(process.env.PORT || 3000, function()
 
 // Create chat bot
 var connector = new builder.ChatConnector
-({ appId: 'YourAppId', appPassword: 'YourAppPassword' }); 
+({ appId: 'd40a9ebf-a0ae-415e-9697-6d9562c64538', appPassword: 'zgqONU9QPSp8Y86jzf167eF' }); 
 var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
 
