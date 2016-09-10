b896d706930a09aaeb77d3101e54f6dbf4297ac5
threebrick <darnell@threebrick.com>
2016-09-10 02:06:38 -0400

    Updated app.js

--- app.js
+++ app.js
@@ -36,6 +36,7 @@ photos, videos, and location.
 
 var restify = require('restify');
 var builder = require('botbuilder');
+var https = require('https');
 
 //=========================================================
 // Bot Setup
@@ -73,10 +74,13 @@ bot.beginDialogAction('help', '/help', { matches: /^help/i });
 // Bots Dialogs
 //=========================================================
 
+var arr = [];  
+  
 function getBooksData(key) {  
     https.get("https://www.googleapis.com/books/v1/volumes?q=" + key + "&maxResults=5", function(res) {  
         var d = '';  
         var i;  
+        arr = [];  
         res.on('data', function(chunk) {  
             d += chunk;  
         });  
@@ -84,6 +88,12 @@ function getBooksData(key) {
             var e = JSON.parse(d);  
             for (i = 0; i < e.items.length; i++) {  
                 console.log(i + 1 + ":" + e.items[i].volumeInfo.title);  
+                arr.push({  
+                    "description": e.items[i].volumeInfo.description,  
+                    "title": e.items[i].volumeInfo.title,  
+                    "saleability": e.items[i].saleInfo.saleability,  
+                    "price": e.items[i].saleInfo.listPrice  
+                });  
             }  
         });  
     });  
@@ -92,13 +102,29 @@ var intents = new builder.IntentDialog();
 bot.dialog('/', intents);  
 intents.matches(/^Hi/i, [  
     function(session) {  
-        builder.Prompts.text(session, 'Hey, I am a BookBot. Welcome to Book Searching through Chat!.To start, which books you would like to search?');  
+        builder.Prompts.text(session, 'Hey, I am a BookBot. Welcome to Book Searching through Chat!  To start, which books you would like to search?');  
     },  
     function(session, results) {  
         session.send('Here are books for topic - %s.', results.response);  
+        var b = [];  
         getBooksData(results.response);  
-    }
+    },  
+    function(session) {  
+        builder.Prompts.text(session, 'which books');  
+    }  
 ]);  
-
-
-intents.onDefault(builder.DialogAction.send('Hi there! How can I help you today?'));  
+intents.matches(/^info?/i, [  
+    function(session) {  
+        builder.Prompts.choice(session, "Which book's info you need?", "1|2|3|4|5");  
+    },  
+    function(session, results) {  
+        var book = arr[results.response.entity - 1];  
+        if (book.saleability == 'FOR_SALE') {  
+            session.send('Title:' + book.title + " Price:" + book.price.amount + " " + book.price.currencyCode);  
+        } else {  
+            session.send('Title:' + book.title + " Price: NOT FOR SALE");  
+        }  
+        session.send('Description:' + book.description);  
+    }  
+]);  
+intents.onDefault(builder.DialogAction.send('Hi there! How can I help you today?')); 
