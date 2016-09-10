73e67c9f8e51b158c2b3c4a5b294dea5cb64f162
threebrick <darnell@threebrick.com>
2016-09-10 01:47:59 -0400

    Updated app.js

--- app.js
+++ app.js
@@ -1,27 +1,104 @@
-// Add your requirements
-var restify = require('restify'); 
-var builder = require('botbuilder'); 
+/*-----------------------------------------------------------------------------
+This Bot uses the Bot Connector Service but is designed to showcase whats 
+possible on Facebook using the framework. The demo shows how to create a looping 
+menu how send things like Pictures, Bubbles, Receipts, and use Carousels. It also
+shows all of the prompts supported by Bot Builder and how to receive uploaded
+photos, videos, and location.
+# RUN THE BOT:
+    You can run the bot locally using the Bot Framework Emulator but for the best
+    experience you should register a new bot on Facebook and bind it to the demo 
+    bot. You can run the bot locally using ngrok found at https://ngrok.com/.
+    * Install and run ngrok in a console window using "ngrok http 3978".
+    * Create a bot on https://dev.botframework.com and follow the steps to setup
+      a Facebook channel. The Facebook channel config page will walk you through 
+      creating a Facebook page & app for your bot.
+    * For the endpoint you setup on dev.botframework.com, copy the https link 
+      ngrok setup and set "<ngrok link>/api/messages" as your bots endpoint.
+    * Next you need to configure your bots MICROSOFT_APP_ID, and
+      MICROSOFT_APP_PASSWORD environment variables. If you're running VSCode you 
+      can add these variables to your the bots launch.json file. If you're not 
+      using VSCode you'll need to setup these variables in a console window.
+      - MICROSOFT_APP_ID: This is the App ID assigned when you created your bot.
+      - MICROSOFT_APP_PASSWORD: This was also assigned when you created your bot.
+    * Install the bots persistent menus following the instructions outlined in the
+      section below.
+    * To run the bot you can launch it from VSCode or run "node app.js" from a 
+      console window. 
+# INSTALL PERSISTENT MENUS
+    Facebook supports persistent menus which Bot Builder lets you bind to global 
+    actions. These menus must be installed using the page access token assigned 
+    when you setup your bot. You can easily install the menus included with the 
+    example by running the cURL command below:
+        curl -X POST -H "Content-Type: application/json" -d @persistent-menu.json 
+        "https://graph.facebook.com/v2.6/me/thread_settings?access_token=PAGE_ACCESS_TOKEN"
+    
+-----------------------------------------------------------------------------*/
+
+var restify = require('restify');
+var builder = require('botbuilder');
+
+//=========================================================
+// Bot Setup
+//=========================================================
 
 // Setup Restify Server
 var server = restify.createServer();
-server.listen(process.env.PORT || 3000, function() 
-{
+server.listen(process.env.port || process.env.PORT || 3978, function () {
    console.log('%s listening to %s', server.name, server.url); 
 });
-
+  
 // Create chat bot
-//var connector = new builder.ChatConnector
-//({ appId: 'd40a9ebf-a0ae-415e-9697-6d9562c64538', appPassword: 'zgqONU9QPSp8Y86jzf167eF' });
-
 var connector = new builder.ChatConnector({
     appId: process.env.MICROSOFT_APP_ID,
     appPassword: process.env.MICROSOFT_APP_PASSWORD
 });
- 
 var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
 
-// Create bot dialogs
-bot.dialog('/', function (session) {
-    session.send("Hello World");
-});
+//=========================================================
+// Bots Middleware
+//=========================================================
+
+// Anytime the major version is incremented any existing conversations will be restarted.
+bot.use(builder.Middleware.dialogVersion({ version: 1.0, resetCommand: /^reset/i }));
+
+//=========================================================
+// Bots Global Actions
+//=========================================================
+
+bot.endConversationAction('goodbye', 'Goodbye :)', { matches: /^goodbye/i });
+bot.beginDialogAction('help', '/help', { matches: /^help/i });
+
+//=========================================================
+// Bots Dialogs
+//=========================================================
+
+function getBooksData(key) {  
+    https.get("https://www.googleapis.com/books/v1/volumes?q=" + key + "&maxResults=5", function(res) {  
+        var d = '';  
+        var i;  
+        res.on('data', function(chunk) {  
+            d += chunk;  
+        });  
+        res.on('end', function() {  
+            var e = JSON.parse(d);  
+            for (i = 0; i < e.items.length; i++) {  
+                console.log(i + 1 + ":" + e.items[i].volumeInfo.title);  
+            }  
+        });  
+    });  
+}  
+var intents = new builder.IntentDialog();  
+bot.dialog('/', intents);  
+intents.matches(/^Hi/i, [  
+    function(session) {  
+        builder.Prompts.text(session, 'Hey, I am a BookBot. Welcome to Book Searching through Chat!.To start, which books you would like to search?');  
+    },  
+    function(session, results) {  
+        session.send('Here are books for topic - %s.', results.response);  
+        getBooksData(results.response);  
+    }
+]);  
+
+
+intents.onDefault(builder.DialogAction.send('Hi there! How can I help you today?'));  
